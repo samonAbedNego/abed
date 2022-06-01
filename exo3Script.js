c51266d35189efe1c8506e814c931435693f3918
samonAbedNego <106690673+samonAbedNego@users.noreply.github.com>
2022-06-01 18:04:26 +0100

    Add files via upload

---
+++ exo3Script.js
@@ -0,0 +1,7 @@
+// Affichage triangulaire
+for(i=1;i<=10;i++){
+    for(j=1;j<=i;j++){
+        console.log(i);
+     
+    }
+} 
---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+    les operations basics de calcul sont: plus(+), fois(*), division(/),soustraction(-),()
+    Nous voullons donc passer à l'utilisation de ces fonctions 
+*/
+// alert("Bonjour entrez votre age s'il vous plait");
+// var age=prompt("Donnez votre ages Svp");
+// alert("vous avez exactement "+age+"ans");
+// alert("Le double de votre age est "+ 2*age);
+// console.log(age);
+// console.log(age)
+var nom=prompt("Bonjour Monsieur c'est quoi votre nom svp");
+var age=prompt("Veuillez entrez votre age s'il vous plait ");
+var objectif=prompt("L'objectif de votre venu:");
+var attent=prompte("Vos attentes svp");
+alert("Merci d'avoir remplir tout les champs " + "VOtre nom est: " + nom + "et vous avez " + age + "ans");
+
